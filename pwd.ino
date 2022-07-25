52e0a7660bc17645f0b98ae8c77ca6f43ff01208
David Chibueze Ndubuisi <davydocsurg@gmail.com>
2022-07-25 03:06:38 +0100

    add password validation check

---
+++ pwd.ino
@@ -0,0 +1,22 @@
+char pwdInput;
+ 
+void setup() {
+    Serial.begin (9600);
+    Serial.setTimeout(50);
+    Serial.println ("----------WELCOME BACK----------");
+    delay (1000);
+    Serial.println ("------------------------------ENTER PASSWORD------------------------------");
+}
+ 
+void loop() {
+    if(Serial.available()){
+        pwdInput = Serial.read();
+        if(sizeof(pwdInput) < 8) {
+            Serial.println ("----------PASSWORD MUST NOT BE LESS THAN 8 CHARACTERS----------");
+        } else if(!isDigit(pwdInput)){
+            Serial.println ("----------PASSWORD MUST CONTAIN AT LEAST ONE NUMBER----------");
+        } else {
+            Serial.println ("----------LOGGED IN----------");
+        }
+    }
+}
